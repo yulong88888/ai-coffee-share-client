08f6965c4af2bc96f483026def93741fbd76d683
yulong <1327417509@qq.com>
2018-04-19 10:37:22 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<swipe :img-datas="imgDatas"/>
+</template>
+
+<script>
+	import Swipe from "./components/swipe/swipe";
+
+	export default {
+		components: {Swipe},
+		name: 'App',
+		data() {
+			return {
+				imgDatas: [
+					"http://img0.imgtn.bdimg.com/it/u=1946406373,1354799675&fm=27&gp=0.jpg",
+					"http://pic1.win4000.com/wallpaper/1/598aac8d2dab2.jpg",
+					"http://img1.imgtn.bdimg.com/it/u=1482437651,683124644&fm=27&gp=0.jpg",
+				]
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/swipe/swipe.vue
@@ -0,0 +1,22 @@
+<template>
+	<mt-swipe :auto="1000" class="swipe-style">
+		<mt-swipe-item v-for="imgData in imgDatas">
+			<a href="www.baidu.com">
+				<img :src="imgData" style="width:100%">
+			</a>
+		</mt-swipe-item>
+	</mt-swipe>
+</template>
+
+<script>
+	export default {
+		name: "swipe",
+		props: ['imgDatas'],
+	}
+</script>
+
+<style lang="stylus">
+	.swipe-style
+		height: 150px;
+		width: 100%;
+</style>
