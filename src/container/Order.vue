0697617e010f5530c062792db9e03875d92af940
yulong88888 <1327417509@qq.com>
2018-04-22 22:39:15 +0800

    继续完善

--- src/container/Order.vue
+++ src/container/Order.vue
@@ -1,5 +1,12 @@
 <template>
-	<div>123</div>
+	<el-container>
+		<el-header>Header</el-header>
+		<el-main>Main</el-main>
+		<el-footer class="foot">
+			<el-button type="text">￥XXX</el-button>
+			<el-button class="pay_btn" type="success">微信支付</el-button>
+		</el-footer>
+	</el-container>
 </template>
 
 <script>
@@ -10,3 +17,13 @@
 		}
 	}
 </script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.foot
+		position: fixed
+		bottom: 0
+		left: 0
+		width: 100%
+		.pay_btn
+			float: right
+</style>
