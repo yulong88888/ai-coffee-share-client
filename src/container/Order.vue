28f8a057a16d95412027eaa27b2945a8f4d3ac42
yulong88888 <1327417509@qq.com>
2018-04-22 22:58:50 +0800

    继续完善

--- src/container/Order.vue
+++ src/container/Order.vue
@@ -3,8 +3,10 @@
 		<el-header>Header</el-header>
 		<el-main>Main</el-main>
 		<el-footer class="foot">
-			<el-button type="text">￥XXX</el-button>
-			<el-button class="pay_btn" type="success">微信支付</el-button>
+			<div style="margin-top: 10px">
+				<el-button type="text">￥XXX</el-button>
+				<el-button class="pay_btn" type="success">微信支付</el-button>
+			</div>
 		</el-footer>
 	</el-container>
 </template>
@@ -20,6 +22,7 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
 	.foot
+		box-shadow: 0px -1px 1px #000
 		position: fixed
 		bottom: 0
 		left: 0
