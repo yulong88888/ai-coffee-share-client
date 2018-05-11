fb4fe1c368f31003273bc2e0e8d73e2aeeca51d2
yulong <1327417509@qq.com>
2018-05-11 15:18:06 +0800

    完善订单页面

--- src/container/Order.vue
+++ src/container/Order.vue
@@ -3,19 +3,33 @@
 		<el-container>
 			<div @click="selectAddress" class="head">
 				<el-header>
-					<el-card>
-						<span class="el-icon-plus" style="color: rgb(255, 128, 0)"></span>
-						<span>添加配送信息</span>
-						<span class="el-icon-arrow-right" style="float: right"></span>
-					</el-card>
+					<div v-if="JSON.stringify(this.accountInfo) === '{}'">
+						<el-card>
+							<span class="el-icon-plus" style="color: rgb(255, 128, 0)"></span>
+							<span>添加配送信息</span>
+							<span class="el-icon-arrow-right" style="float: right"></span>
+						</el-card>
+					</div>
+					<div v-else>
+						<el-card>
+							<span style="float: left">
+								<div>姓名：{{this.accountInfo.name}}</div>
+								<div>电话：{{this.accountInfo.phone}}</div>
+								<div>地址：{{this.accountInfo.address}}</div>
+							</span>
+							<span class="el-icon-arrow-right" style="float: right"></span>
+						</el-card>
+					</div>
+
 				</el-header>
 			</div>
 			<el-main>
 				<el-card class="big-card">
-					<div v-for="o in 4" :key="o">
+					<div v-for="obj in buyProducts" :key="obj.id">
 						<div class="item text">
-							<span>{{'列表内容 ' + o }}</span>
-							<span style="float: right">￥</span>
+							<span>{{ obj.name }}</span>
+							<span>x{{ obj.count }}</span>
+							<span style="float: right">{{ (obj.price*obj.count).toFixed(2) }}￥</span>
 						</div>
 						<hr/>
 					</div>
@@ -31,7 +45,7 @@
 					<div class="text item">
 						<span>总价</span>
 						<span style="float: right">还需支付
-							<span style="font-size: 20px">￥66</span>
+							<span style="font-size: 20px">￥{{totalPrice.toFixed(2)}}</span>
 						</span>
 					</div>
 				</el-card>
@@ -46,7 +60,7 @@
 			</el-main>
 			<el-footer class="foot">
 				<div style="margin-top: 10px">
-					<el-button type="text">￥66</el-button>
+					<el-button type="text">￥{{totalPrice.toFixed(2)}}</el-button>
 					<el-button class="pay_btn" type="success" @click="toPay">微信支付</el-button>
 				</div>
 			</el-footer>
@@ -69,7 +83,11 @@
 		name: "order",
 		data() {
 			return {
-				dialogVisible: false
+				//购买的产品
+				buyProducts: [],
+				accountInfo: {},
+				totalPrice: 0,
+				dialogVisible: false,
 			}
 		},
 		created() {
@@ -78,12 +96,47 @@
 		methods: {
 			selectAddress() {
 				console.log("selectAddress");
+				this.$router.push({
+					path: '/info',
+				});
 			},
 			selectCoupon() {
 				console.log("selectCoupon");
-			}, toPay() {
+			},
+			toPay() {
 				console.log("toPay");
-			}
+			},
+			// // 取到路由带过来的参数
+			// getParams() {
+			// 	let routerParams = this.$route.params.obj;
+			// 	// 将数据放在当前组件的数据内
+			// 	console.error(routerParams);
+			// 	// this.msg = routerParams
+			// }
+		},
+		// watch: {
+		// 	'$route': 'getParams'
+		// },
+		created() {
+			console.log(this.$route.query);
+			this.accountInfo = this.$route.query;
+			// 获取购买的东西
+			this.$axios.post("./api/shopCart/get").then(response => {
+				if (response.data.code === 0) {
+					console.log("加载服务器Session商品", response.data.recdata);
+					if (typeof (response.data.recdata) + "" === "undefined") {
+						this.buyProducts = [];
+						return;
+					}
+					this.buyProducts = response.data.recdata;
+					//计算总价
+					let temp = 0;
+					this.buyProducts.forEach(function (obj) {
+						temp += obj.price * obj.count;
+					});
+					this.totalPrice = temp;
+				}
+			});
 		}
 	}
 </script>
@@ -98,7 +151,8 @@
 
 	.head
 		width: 100%
-		box-shadow: 0px 1px 5px #CCC
+
+	/*box-shadow: 0px 1px 5px #CCC*/
 
 	.foot
 		box-shadow: 0px -1px 1px #CCC
