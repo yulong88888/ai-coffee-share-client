962c262e9f6b74c03327ec687bcc261606c0db5f
yulong <1327417509@qq.com>
2018-05-12 17:56:31 +0800

    优化文件

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,6 @@ Vue.use(vuex);
 export default new vuex.Store({
 	state: {
 		userBaseInfo: {},
-		baseInfo: {},
-		productInfo: {},
 		// count: 0
 	},
 	mutations: {
@@ -15,16 +13,6 @@ export default new vuex.Store({
 			console.error("store", payload);
 			let temp = state;
 			temp.userBaseInfo = payload;
-		},
-		setBaseInfoMutations: (state, payload) => {
-			console.error("store", payload);
-			let temp = state;
-			temp.baseInfo = payload;
-		},
-		setProductInfoMutations: (state, payload) => {
-			console.error("store", payload);
-			let temp = state;
-			temp.productInfo = payload;
 		},
 		// increment: state => state.count++,
 		// decrement: state => state.count--
@@ -34,24 +22,10 @@ export default new vuex.Store({
 			console.log("store getters userBaseInfo", state.userBaseInfo);
 			return state.userBaseInfo
 		},
-		baseInfo: state => {
-			console.log("store getters baseInfo", state.baseInfo);
-			return state.baseInfo
-		},
-		productInfo: state => {
-			console.log("store getters productInfo", state.productInfo);
-			return state.productInfo
-		}
 	},
 	actions: {
 		setUserBaseInfo(context, userBaseInfo) {
 			context.commit('setUserBaseInfoMutations', userBaseInfo);
-		},
-		setBaseInfo(context, baseInfo) {
-			context.commit('setBaseInfoMutations', baseInfo);
-		},
-		setProductInfo(context, productInfo) {
-			context.commit('setProductInfoMutations', productInfo);
 		},
 		// test(context) {
 		// 	context.commit('increment');
