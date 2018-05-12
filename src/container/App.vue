f95dc8be1d5f70a97f5193496fe50a65217be31f
yulong <1327417509@qq.com>
2018-05-12 17:58:57 +0800

    优化文件

--- src/container/App.vue
+++ src/container/App.vue
@@ -25,24 +25,6 @@
 				}).then(response => {
 					console.log(response.data.recdata);
 					this.$store.dispatch('setUserBaseInfo', response.data.recdata);
-					//this.$store.dispatch('test');
-					//console.log(global.userBaseInfo);
-
-					// //获取设置商户基本信息
-					// this.$axios.get("./api/baseInfo/get").then(response => {
-					// 	if (response.data.code === 0) {
-					// 		console.log(response.data.recdata);
-					// 		this.$store.dispatch('setBaseInfo', response.data.recdata);
-					// 	}
-					// });
-
-					// //获取设置产品基本信息
-					// this.$axios.get("./api/product/get").then(response => {
-					// 	if (response.data.code === 0) {
-					// 		console.log(response.data.recdata);
-					// 		this.$store.dispatch('setProductInfo', response.data.recdata);
-					// 	}
-					// });
 				});
 			}
 		}
