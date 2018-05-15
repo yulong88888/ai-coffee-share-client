21af80a0f48df529a9f2a503327dc466d4667b72
yulong <1327417509@qq.com>
2018-05-15 17:40:49 +0800

    新增假数据

--- src/container/admin_orders.vue
+++ src/container/admin_orders.vue
@@ -1,29 +1,49 @@
 <template>
 	<div>
 		<el-collapse accordion>
-			<div v-for="orders_item in orders">
-				<el-collapse-item :title="orders_item.state">
-					<el-table :data="orders" style="width: 100%">
-						<el-table-column prop="orderId" label="订单编号"/>
-						<!--<el-table data="accountInfo" label="买家">-->
-						<el-table-column prop="accountInfo.name" label="姓名"/>
-						<el-table-column prop="accountInfo.phone" label="电话"/>
-						<el-table-column prop="accountInfo.address" label="地址"/>
-						<!--</el-table>-->
-						<el-table :data="orders.orderInfo" label="购买内容" style="width: 100%">
-							<el-table-column prop="name" label="名称"/>
-							<el-table-column prop="count" label="个数"/>
-						</el-table>
-						<el-table-column label="操作">
-							<template slot-scope="scope">
-								<el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-								<el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除
-								</el-button>
-							</template>
-						</el-table-column>
-					</el-table>
-				</el-collapse-item>
-			</div>
+			<!--<div v-for="orders_item in orders">-->
+			<!--<el-collapse-item :title="orders_item.state">-->
+			<el-table :data="orders" style="width: 100%">
+				<el-table-column prop="orderId" label="订单编号"/>
+				<el-table-column label="买家信息">
+					<el-table-column prop="accountInfo.name" label="姓名"/>
+					<el-table-column prop="accountInfo.phone" label="电话"/>
+					<el-table-column prop="accountInfo.address" label="地址"/>
+				</el-table-column>
+				<el-table-column prop="orderInfo" label="购买内容" style="width: 100%">
+					<el-table-column label="名称">
+						<template slot-scope="scope">
+							<div v-for="orderInfoItem in scope.row.orderInfo">
+								{{orderInfoItem.name}}<br/>
+							</div>
+						</template>
+					</el-table-column>
+					<el-table-column label="数量">
+						<template slot-scope="scope">
+							<div v-for="orderInfoItem in scope.row.orderInfo">
+								{{orderInfoItem.count}}<br/>
+							</div>
+						</template>
+					</el-table-column>
+
+					<!--<template slot-scope="scope">-->
+					<!--<el-table :data="scope.row.orderInfo">-->
+					<!--<el-table-column prop="name" label="产品名称"/>-->
+					<!--<el-table-column prop="count" label="数量"/>-->
+					<!--</el-table>-->
+					<!--</template>-->
+
+				</el-table-column>
+				<el-table-column label="操作">
+					<template slot-scope="scope">
+						<el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+						<el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除
+						</el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<!--</el-collapse-item>-->
+			<!--</div>-->
 		</el-collapse>
 	</div>
 </template>
